781d47fa9c60ef5fa1be5728859dc68d0612a6da
Darren du preez <darren.dupreez@gmail.com>
2020-09-29 23:11:26 -0400

    fixed spaces in cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,11 +12,9 @@ df = pd.read_csv('glassdoor_jobs.csv')
 df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
 df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary' in x.lower() else 0)
 
-
 df = df[df['Salary Estimate'] != '-1']
 salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
 minus_Kd = salary.apply(lambda x: x.replace('K', '').replace('CA$', ''))
-
 
 min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour', '').replace('employer provided salary:', ''))
 
@@ -37,7 +35,6 @@ df['city'] = df['Location']
 df['age'] = df.Founded.apply(lambda x: x if x <1 else 2020 - x)
 
 # parsing of job description (python, etc.)
-
 
 #python
 df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
